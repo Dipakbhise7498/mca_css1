52cdbec032ad7af5c32b1beae530f8365f0a010b
Dipak Bhise <dipakbhise7498@gmail.com>
2024-02-22 14:25:40 +0530

    latest code push

--- inherit_demo.py
+++ inherit_demo.py
@@ -2,13 +2,13 @@ class Student:
     def __init__(self,m1,m2):
         self.m1=m1
         self.m2=m2
-    def __add(self,other):
+    def __add__(self,other):
        s1=self.m1+other.m1
-       s=self.m2+other.m2
+       s2=self.m2+other.m2
        s3=Student(s1,s2)
        return s3
-s1=Student(m1:10, m2:20)
-s2=Student(m1:20, m2:30)
+s1=Student(10,20)
+s2=Student(20,30)
 s3=s1+s2  
 print(s3.m1)
 print(s3.m2)
