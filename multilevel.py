6bbcf007cb22d8b315773b2091407f1d3b3eee62
Dipak Bhise <dipakbhise7498@gmail.com>
2024-02-22 14:20:38 +0530

    latest code push

--- multilevel.py
+++ multilevel.py
@@ -1,11 +1,13 @@
 class A:
     def__init__(self):
-    #def feature1(self):#
         print("__init__of class A constructor")    
+    def feature1(self):
+        print("feature 1 is working")
     def feature2(self):
         print("feature 2 is working")  
                 
 class B(A):
+    def__init__(self):
     def feature3(self):
         print("feature 3 is working")
         
